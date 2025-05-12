e18eabf5a7d36eae01aa40305f87de31415655fa
qunik <uuuvakrn@gmail.com>
2025-05-12 21:32:32 +0300

    Перше завантаження

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('playBtn').addEventListener('click', function () {
+  const video = document.getElementById('myVideo');
+  video.style.display = 'block';
+  video.play();
+});
